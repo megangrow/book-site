00471711295991112d43923206b4530f8dcb5eef
megan <megangrow05@gmail.com>
2026-02-04 13:38:56 -0600

    Add Vuetify and additional pages

--- src/audiobooks.vue
+++ src/audiobooks.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <h1>Home Page</h1>
+<h1>Audiobooks</h1>
 </template>
 
 <style scoped>
---
+++ src/library.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Library</h1>
+  <h2>Currently Reading</h2>
+  <h2>Past Reads</h2>
+  <h2>To Be Read</h2>
+</template>
+
+<style scoped>
+
+</style>
